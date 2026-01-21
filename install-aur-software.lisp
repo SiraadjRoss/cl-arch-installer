0f2e59b25e7647cc3e61c371f4d7dc59926d4273
SiraadjRoss <73166557+SiraadjRoss@users.noreply.github.com>
2026-01-21 19:26:11 +0300

    Update install-aur-software.lisp

--- install-aur-software.lisp
+++ install-aur-software.lisp
@@ -1,1 +1,27 @@
+#!/usr/bin/sbcl --script
+
+;;;; Brave browser installer (conceptual, educational use only!)
+(require :sb-posix)
+
+(defpackage :brave-browser-install
+  (:use :cl :sb-ext)
+  (:import-from :sb-posix :chdir)
+  (:export :main))
+(in-package :brave-browser-install)
+
+
+(defun run-command (cmd &key (output t) (error-output t))
+  "Execute shell command. cmd is string"
+  (let ((exit-code (sb-ext:process-exit-code
+                    (sb-ext:run-program "/bin/sh"
+                                        (list "-c" cmd)
+                                        :output output
+                                        :error error-output
+                                        :wait t))))
+    (unless (zerop exit-code)
+      (error "Command failed: ~A (exit code ~A)" cmd exit-code))))
+
+(defun run-user-command (cmd)
+  (let ((preffix "su - siraadj -c"))
+    (run-command (concatenate 'string preffix (format nil " ~s" cmd)))))
 
